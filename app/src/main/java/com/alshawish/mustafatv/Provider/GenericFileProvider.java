ad1c4641860c9d905ad1bdec3f2351b370390ecd
Majd Sharabah <MajdSharabah8396@gmail.com>
2022-01-20 23:06:33 +0300

    First commit

---
+++ app/src/main/java/com/alshawish/mustafatv/Provider/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.alshawish.mustafatv.Provider;
+
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+
+}
---
+++ app/src/main/java/com/alshawish/mustafatv/config/Global.java
@@ -0,0 +1,25 @@
+package com.alshawish.mustafatv.config;
+
+/**
+ * Created by Tamim on 28/09/2017.
+ */
+
+
+
+public class Global {
+    public static final String API_URL = "https://admin.mustafa-tv.com/api/";
+    public static final String SECURE_KEY = "4F5A9C3D9A86FA54EACEDDD635185";
+
+    public static final String Youtube_Key = "AIzaSyAephi0fVTEBXgphX7Z_WVSW8iPusDibtg"; // get it from this link  https://console.developers.google.com/apis/credentials
+
+    public static final String ITEM_PURCHASE_CODE = "03c4145b-3999-470e-ba01-8c4bd7ae3f21";
+
+
+
+    public static final String SUBSCRIPTION_ID = "SUBSCRIPTION_ID";
+    public static final String MERCHANT_KEY = "MERCHANT_KEY" ; // PUT YOUR MERCHANT KEY HERE;
+    public static final long   SUBSCRIPTION_DURATION = 30; // PUT SUBSCRIPTION DURATION DAYS HERE;
+}
+
+
+
